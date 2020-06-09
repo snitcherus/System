ccf43c8f6511e6d9277fdbb61537e68c2ded883c
Snitcherus <snitch@snitcherus.de>
2020-06-09 10:05:15 +0200

    Added EnderChestCmd Cooldown

--- src/main/java/de/snitchi/system/commands/EnderChestCmd.java
+++ src/main/java/de/snitchi/system/commands/EnderChestCmd.java
@@ -1,6 +1,7 @@
 package de.snitchi.system.commands;
 
 import de.snitchi.system.util.ResourceMessage;
+import java.util.HashMap;
 import org.bukkit.Sound;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -10,6 +11,8 @@ import org.bukkit.entity.Player;
 public class EnderChestCmd implements CommandExecutor {
 
   private final ResourceMessage resourceMessage;
+
+  private HashMap<String, Long> cooldown = new HashMap<String, Long>();
 
   public EnderChestCmd() {
     this.resourceMessage = new ResourceMessage();
@@ -28,8 +31,26 @@ public class EnderChestCmd implements CommandExecutor {
       return true;
     }
 
-    player.playSound(player.getLocation(), Sound.BLOCK_CHEST_OPEN, 1, 1);
-    player.openInventory(player.getEnderChest());
+    int coolDownTime = 5;
+
+    if (!cooldown.containsKey(player.getName())) {
+      cooldown.put(player.getName(), System.currentTimeMillis());
+      player.playSound(player.getLocation(), Sound.BLOCK_CHEST_OPEN, 1, 1);
+      player.openInventory(player.getEnderChest());
+      return true;
+    }
+
+    long secondsLeft =
+        ((cooldown.get(sender.getName()) / 1000) + coolDownTime) - (System.currentTimeMillis()
+            / 1000);
+
+    if (secondsLeft > 0) {
+      resourceMessage.sendMessage(player, "command.counting", secondsLeft);
+      return true;
+    }
+
+    cooldown.remove(player.getName());
+
     return true;
   }
 }
